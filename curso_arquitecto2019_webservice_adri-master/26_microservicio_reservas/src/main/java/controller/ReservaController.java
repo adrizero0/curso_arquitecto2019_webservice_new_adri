4f93815415f0ffa07a70c7697da04ec1702df68e
Enrique <adriana@home.com>
2019-11-30 23:31:24 +0100

    comm5

--- curso_arquitecto2019_webservice_adri-master/26_microservicio_reservas/src/main/java/controller/ReservaController.java
+++ curso_arquitecto2019_webservice_adri-master/26_microservicio_reservas/src/main/java/controller/ReservaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
--- curso_arquitecto2019_webservice_adri-master/26_microservicio_vuelos/src/main/java/inicio/Application.java
+++ curso_arquitecto2019_webservice_adri-master/26_microservicio_vuelos/src/main/java/inicio/Application.java
@@ -3,8 +3,11 @@ package inicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @ComponentScan(basePackages = {"controller", "service"})
 @EntityScan(basePackages = {"model"})
@@ -15,5 +18,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
 }
